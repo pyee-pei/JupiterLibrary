896d56fa47be810767ad90314377791b8fe2ea95
Peter Yee <peter.yee@pe-impact.com>
2023-01-09 18:06:34 -0600

    20230109

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,24 +2,70 @@
 
 // Description: Utility functions for ThoughtTrace document objects
 
+/***
+ * extract multiple facts from a fact type
+ */
+function extractMultiFactValues (doc, factType) {
+  
+  const facts = doc.facts ? doc.facts.filter(x => x.factTypeId === factType) : null;
+  
+  // do not proceed if no facts found
+  if (!facts) { return []; }
+    
+  const result = [];
+  
+  facts.forEach((f) => {
+    
+    const fact = {};
+    fact.id = f.id;
+    
+    if (!f.fields) { return fact; }
+    
+    f.fields.forEach((field) => {
+      
+      var property_name = cleanFieldNames(field.factTypeFieldName);
+      var value = '';
+      
+      switch(field.dataType) {
+        case 'Number': value = field.numberValue; break;
+        case 'Date': value = new luxon.DateTime.fromISO(field.dateValue); break;
+        case 'String': value = field.stringValue; break;
+        case 'SelectList': value = field.stringValue; break;
+        case 'Boolean': value = field.booleanValue; break;
+      }
+      
+      // create property and value based on results
+      fact[property_name] = value;
+      
+    });
+    
+    result.push(fact);
+    
+  });
+  
+  return result;
+
+}
+
+
+/***
+ * clean unused fields from document object
+ * just makes debugging easier
+ */
 function cleanDoc(doc) {
-  // remove clutter
-  delete doc.archivedBy;
-  delete doc.archivedOn;
-  delete doc.highlightedText;
-  delete doc.pageManipulationStatus;
-  delete doc.processingStatus;
-  delete doc.pages;
-  delete doc.securityLabelId;
-  delete doc.sourceDocumentId;
-  delete doc.sourceDocumentName;
-  delete doc.pageCount;
-  delete doc.thoughts;
-  delete doc.userIds;
+
+  const fields = ['archivedBy', 'archivedOn', 'highlightedText', 'pageManipulationStatus', 'processingStatus', 'pages', 'securityLabelId', 'sourceDocumentId', 'sourceDocumentName', 'pageCount', 'thoughts', 'userIds'];
+
+  fields.forEach(field => {
+    delete doc[field];
+  });
 
   return doc;
 }
 
+/***
+ * convert fact field names to be more javascript friendly
+ */
 function cleanFieldNames(str) {
   // convert field name text to property names
   return str
@@ -28,33 +74,44 @@ function cleanFieldNames(str) {
     .replace(/_+$/, "");
 }
 
-var utils = { cleanDoc, cleanFieldNames };
+var utils = { extractMultiFactValues, cleanDoc, cleanFieldNames };
 
-/**
- * Represents a Jupiter document.
+const FACT_TYPE_IDS = {
+    lease_terms: '7601840f-8d47-4c0b-a2a1-2ad2a692bfb8'
+};
+
+/** 
+* Represents a Jupiter document.
  * @constructor
  * @param {Object} doc - The document object itself
+ * @property {string} id - Unique identifier (ThoughtTrace)
+ * @property {Array} lease_terms - Lease terms
  */
-function Constructor(doc) {
-  // remove clutter
-  utils.cleanDoc(doc);
-  this.doc = doc;
+class JupiterDoc {
+    constructor(doc) {
+        // remove clutter
+        utils.cleanDoc(doc);
+
+        // set properties
+        this.docObject = doc;
+        this.id = doc.id;
+
+        this.lease_terms = utils.extractMultiFactValues(doc, FACT_TYPE_IDS.lease_terms);
+    }
+    /**
+     * Returns the document id.
+     * @returns {string} - The document id
+     */
+    getId() {
+        return this.id;
+    }
+    /**
+     * Returns the document name.
+     * @returns {string} - The document name
+     */
+    getName() {
+        return this.name;
+    }
 }
 
-/**
- * Returns the document id.
- * @returns {string} - The document id
- */
-Constructor.prototype.getId = function () {
-  return this.doc.id;
-};
-
-/**
- * Returns the document name.
- * @returns {string} - The document name
- */
-Constructor.prototype.getName = function () {
-  return this.doc.name;
-};
-
-module.exports = Constructor;
+module.exports = JupiterDoc;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
 import JupiterDoc from "./jupiter.js";
 
-const myDoc = new JupiterDoc({
-  id: 12345,
-  name: "test",
-  pages: [1, 2, 3],
+var docs = [];
+var factTypes = [];
+
+const jupiterDocs = [];
+
+// fetch data from json files and put them into variables
+const getDocs = () => {
+    return fetch("../data/documents.json")
+        .then(res => res.json())
+        .then((data) => {
+            return data;
+        });
+}
+
+const getFactTypes = () => {
+    return fetch("../data/factTypes.json")
+        .then(res => res.json())
+        .then((data) => {
+            return data;
+        });
+}
+
+// await both fetches to complete with Promise.all
+
+const getDocsAndFactTypes = async () => {
+    docs = await getDocs();
+    factTypes = await getFactTypes();
+    return { docs, factTypes };
+}
+
+// wait until arrays are filled
+await getDocsAndFactTypes();
+
+docs.forEach((doc) => {
+    jupiterDocs.push(new JupiterDoc(doc, factTypes));
 });
 
-console.log(myDoc);
+console.log(jupiterDocs);
 
 export default {};
